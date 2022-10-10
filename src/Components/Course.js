22c24787c5bc303150f961a25e0de527ef94b2e2
sampod76 <sampodnath76@gmail.com>
2022-10-11 05:11:31 +0600

    add click right and wrong quiz count

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Question from './Question';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+
+export const RightAnswer = createContext(0)
+export const WrongAnswer = createContext(0)
 
 const Course = () => {
     const getCoursData = useLoaderData()
     const { name, total, logo, questions } = getCoursData.data
+    const [right, setRight] = useState([])
+    const [wrong, setWrong] = useState([])
 
-    console.log(getCoursData)
+    // console.log(getCoursData)
     return (
-        <div>
-            <div>
-                <h1 className=' bg-cyan-200 p-5 my-3 rounded-lg text-2xl font-semibold shadow-xl w-fit mx-auto'>Course Name: {name}</h1>
-               <div>
-               <h1 className='bg-cyan-600 p-3 rounded-md text-gray-600 text-lg font-semibold text-center'>Please solution this Quiz</h1>
-              <h1 className='text-5xl text-center'> <FontAwesomeIcon icon={faArrowDown} /></h1>
-               </div>
-            </div>
-            <section className=''>
-                {
-                    questions.map((question,index)=><Question key={question.id} question={question} index={index}></Question>)
-                }
-            </section>
-        </div>
+        <RightAnswer.Provider value={[right, setRight]}>
+            <WrongAnswer.Provider value={[wrong, setWrong]}>
+
+                <div className=''>
+                  
+                        <div className='flex sticky top-0'>
+                            <h1 className=' bg-cyan-200 p-5 my-3 rounded-lg text-2xl font-semibold shadow-xl w-fit mx-auto'>Course Name: {name}</h1>
+                            <h1 className=' bg-cyan-200 p-5 my-3 rounded-lg text-2xl font-semibold shadow-xl w-fit mx-auto'>Right Quiz: {right.length}</h1> <h1 className='text-red-600 bg-cyan-200 p-5 my-3 rounded-lg text-2xl font-semibold shadow-xl w-fit mx-auto'>Wrong Wrong: {wrong.length}</h1></div>
+
+                        <div>
+                            <h1 className='bg-cyan-600 p-3 rounded-md text-gray-600 text-lg font-semibold '>Please solution this Quiz </h1>
+                            <h1 className='text-5xl text-center'> <FontAwesomeIcon icon={faArrowDown} /></h1>
+                        </div>
+                  
+                    <section className=''>
+                        {
+                            questions.map((question, index) => <Question key={question.id} question={question} index={index}></Question>)
+                        }
+                    </section>
+                </div>
+            </WrongAnswer.Provider>
+        </RightAnswer.Provider>
     );
 };
 
--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 import Swal from 'sweetalert2'
+import { RightAnswer, WrongAnswer } from './Course';
 
 
 const Question = ({ question, index }) => {
     const { question: name, correctAnswer, options } = question
     const [isActive, setIsActive] = useState(true);
+    const [right, setRight]=useContext(RightAnswer)
+    const [wrong,setWrong]=useContext(WrongAnswer)
     
     const handelAnswer = () => {
         Swal.fire(
@@ -14,7 +17,7 @@ const Question = ({ question, index }) => {
             `${correctAnswer}`
         )
     }
-    const handelRightAnswer =(ans,e)=>{
+    const handelRightAnswer =(ans,name)=>{
 
     if(ans === correctAnswer){
         Swal.fire({
@@ -25,6 +28,13 @@ const Question = ({ question, index }) => {
             timer: 1500
           })
           setIsActive(true);
+
+          const rightFound=right.find(ri=>correctAnswer==ri)
+       
+          if(!rightFound){
+            const rest =[...right,correctAnswer]
+            setRight(rest)
+          }
      
     }else{
         Swal.fire({
@@ -34,6 +44,12 @@ const Question = ({ question, index }) => {
             
           })
           setIsActive(false);
+
+          const wrongFound=wrong.find(wro=>wro==name)
+          if(!wrongFound){
+            const rest =[...wrong,name]
+            setWrong(rest)
+          }
     }
 
     }
@@ -50,7 +66,7 @@ const Question = ({ question, index }) => {
             <div className='text-left p-5 cursor-pointer'>
                 {
                     options.map((ans, index) => <li 
-                    onClick={()=>handelRightAnswer(ans)}
+                    onClick={()=>handelRightAnswer(ans,name)}
                     className={`capitalize p-2 my-2 rounded-lg text-white hover:bg-blue-700 hover:text-white hover:rounded-lg ${isActive ? 'bg-green-800' : 'bg-red-700'}`}
                     key={index}
                     ans={ans}>
