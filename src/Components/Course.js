5d3053258af946ef513f6840c069a8722668ceb2
sampod76 <sampodnath76@gmail.com>
2022-10-11 01:29:17 +0600

    add right answer toest/sweetaleart

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -15,7 +15,7 @@ const Course = () => {
                 <h1 className=' bg-cyan-200 p-5 my-3 rounded-lg text-2xl font-semibold shadow-xl w-fit mx-auto'>Course Name: {name}</h1>
                <div>
                <h1 className='bg-cyan-600 p-3 rounded-md text-white text-lg font-semibold'>Please solution this question</h1>
-              <h1 className='h1'> <FontAwesomeIcon icon={faArrowDown} /></h1>
+              <h1 className='h1 text-center'> <FontAwesomeIcon icon={faArrowDown} /></h1>
                </div>
             </div>
             <section className=''>
--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,30 +1,59 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faEye } from '@fortawesome/free-solid-svg-icons'
-  import Swal from 'sweetalert2'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import Swal from 'sweetalert2'
 
-const Question = ({question, index}) => {
-    const {question:name ,correctAnswer,options}=question
- const handelAnswer =()=>{
-    Swal.fire(
-        'The quiz right answer',
-        `${correctAnswer}`
-      )
- }
+const Question = ({ question, index }) => {
+    const { question: name, correctAnswer, options } = question
+    const handelAnswer = () => {
+        Swal.fire(
+            'The quiz right answer',
+            `${correctAnswer}`
+        )
+    }
+    const handelRightAnswer =(ans)=>{
 
-// console.log(question)
+    if(ans == correctAnswer){
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+            
+          })
+    }
+
+    }
+
+    // console.log(question)
     return (
         <div className='rounded-lg my-5 text-justify p-5 bg-slate-100 shadow-xl'>
-           <div className=' flex justify-between text-xl font-semibold '>
-           <h1 className='w-[90%]'>Quiz {index+1} {name}</h1>
-          
-           <h1 className=''><FontAwesomeIcon onClick={handelAnswer} icon={faEye} /></h1>
-           </div>
-            
+            <div className=' flex justify-between text-xl font-semibold '>
+                <h1 className='w-[90%]'>Quiz {index + 1} {name}</h1>
+
+                <h1 className=''><FontAwesomeIcon onClick={handelAnswer} icon={faEye} /></h1>
+            </div>
+
             <div className='text-left p-5'>
-         {
-            options.map((ans,index) => <li className='capitalize p-2 hover:bg-blue-700 hover:text-white hover:rounded-lg' key={index} ans={ans}>{ans}</li>)
-         }
+                {
+                    options.map((ans, index) => <li 
+                    onClick={()=>handelRightAnswer(ans)}
+                    className='capitalize p-2 hover:bg-blue-700 hover:text-white hover:rounded-lg'
+                    key={index}
+                    ans={ans}>
+                        {ans}
+                    
+                    
+                    
+                    </li>)
+                }
             </div>
         </div>
     );
