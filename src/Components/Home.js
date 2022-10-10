1edf8520dcf670c4b3cfd812381c55763456b028
sampod76 <sampodnath76@gmail.com>
2022-10-11 02:45:14 +0600

    add web site title and icon

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from './Quiz';
 
+
 const Home = () => {
     const getQuizs = useLoaderData().data
     // console.log(getQuiz)
@@ -9,6 +10,9 @@ const Home = () => {
         <div>
             <div className=' bg-cyan-200 p-5 my-3 rounded-lg text-2xl font-semibold shadow-xl '>
                 <h1>All the courses to be Full Stack Development will be found here</h1>
+                <div>
+                    
+                </div>
             </div>
             <div className='grid grid-cols-3 gap-2 my-4'>
                 {
