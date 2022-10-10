2c5a32df720f6d12a0f83a0ccb9b3a3b3d7b06e4
sampod76 <sampodnath76@gmail.com>
2022-10-11 02:31:44 +0600

    add recharts to fontawesomeIcon

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -5,6 +5,8 @@ import {
     BarChart, XAxis, YAxis, Bar
 } from 'recharts';
 import Quiz from './Quiz';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
 
 const Statistics = () => {
     const getQuizData = useLoaderData().data
@@ -16,6 +18,7 @@ const Statistics = () => {
 
         <div>
             <h1 className='button mt-5'>Recharts For the full stack developer course Quiz</h1>
+            <h1 className='text-5xl text-center'> <FontAwesomeIcon icon={faArrowDown} /></h1>
 
             <BarChart width={500} height={500} data={data}>
                 <XAxis dataKey="name"></XAxis>
